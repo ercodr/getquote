5a6903e0462a3385d20aef780e5ff4c0344ce222
ercodr <82173838+Dahcodingkid@users.noreply.github.com>
2021-09-09 06:55:00 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const bookmarkPage = document.getElementById('bookmark');
 toggleTheme.addEventListener('click', ()=> {
     document.getElementById('appBar').classList.toggle('toggle-app-bar');
     document.getElementById('footer').classList.toggle('toggle-footer');
-    document.getElementById('content').classList.toggle('toggle-content');
+    document.getElementById('body').classList.toggle('toggle-body');
     document.getElementById('displayCard').classList.toggle('toggle-displayCard');
 });
 
